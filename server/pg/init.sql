64f99dc23236eed2e378061d32152e48242436d0
Sida Wang <42928828+sida-wang@users.noreply.github.com>
2022-11-11 00:11:33 +1100

    Updating schema

--- server/pg/init.sql
+++ server/pg/init.sql
@@ -4,7 +4,7 @@ CREATE TABLE posts(
     id SERIAL PRIMARY KEY,
     title TEXT NOT NULL, 
     content TEXT,
-    slug TEXT NOT NULL,
+    slug TEXT NOT NULL UNIQUE,
     created_at TIMESTAMPTZ NOT NULL,
     modified_at TIMESTAMPTZ NOT NULL
 );
@@ -13,7 +13,7 @@ CREATE TABLE tags(
     id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
     meta_title TEXT,
-    slug TEXT NOT NULL
+    slug TEXT NOT NULL UNIQUE
 );
 
 CREATE TABLE post_tags(
