af8889a4c834c673dd95e29e5197c2bb6609891f
Sida Wang <42928828+sida-wang@users.noreply.github.com>
2022-11-22 11:51:32 +1100

    Adding link from error page back to home

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,10 +1,24 @@
 import Button from 'react-bootstrap/Button'
+import { Link } from 'react-router-dom'
 import './Filter.css'
+import { getTagsAll } from '../util/apiCalls'
+import { useState, useEffect } from 'react'
 
 const Filter = () => {
+
+  const [filters, setFilters] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const jsonData = await getTagsAll();
+      setFilters(jsonData);
+      })();
+    }
+  ,[]);
+
   return (
     <div className='Filter'>
-        <Button className="btn btn-sm btn-secondary">Secondary</Button>
+        {filters.map((tag) => (<Link to={`/tags/${tag.id}`} key={tag.id} role="button" className="btn btn-secondary">{tag.title}</Link>))}  
     </div>
   )
 }
