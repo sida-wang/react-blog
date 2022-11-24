b0c0def066f74a371c02e704084401136c035346
Sida Wang <42928828+sida-wang@users.noreply.github.com>
2022-11-24 14:11:38 +1100

    Fixing footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,17 @@
 import './Footer.css'
 import { Link } from 'react-router-dom'
+import Button from 'react-bootstrap/Button';
 
 const Footer = () => {
   return (
-    <>
-    <div className='footer'>Footer <Link to={"/admin"}>Admin Dashboard</Link></div>
-    </>
-
+    <div className='footer'>
+    <div></div>
+    <h5 className='m-0 grid-middle'> &copy; 2022 Sida Wang </h5>
+    <Link to={"/admin"} className='grid-right px-3'>
+      <Button type="button" className="btn btn-secondary">Admin Dashboard
+      </Button>
+    </Link>
+    </div>
   )
 }
 
