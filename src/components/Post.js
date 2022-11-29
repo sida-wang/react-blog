b167681c767dcf4a8383d0aa484bc13f1e6bff6f
Sida Wang <42928828+sida-wang@users.noreply.github.com>
2022-11-29 21:34:36 +1100

    Fixing post bug from refactoring

--- src/components/Post.js
+++ src/components/Post.js
@@ -5,8 +5,8 @@ import './Post.css'
 const Post = ({ id, post }) => {
   return (
     <article className='p-2 rounded post text-dark'>
-        <h3>{post.title}</h3>
-        <div className='d-flex flex-wrap'>
+        <h3 className='mb-1'>{post.title}</h3>
+        <div className='d-flex flex-wrap mb-2'>
         <div className='text-secondary font-monospace text fs-7 me-4'>
           Created: {post.created_at.toLocaleString('en-AU', {timeZone: 'Australia/Melbourne', hour12: false, dateStyle: "short", timeStyle: "short"})}    
         </div>
@@ -14,7 +14,7 @@ const Post = ({ id, post }) => {
           Modified: {post.modified_at.toLocaleString('en-AU', {timeZone: 'Australia/Melbourne', hour12: false, dateStyle: "short", timeStyle: "short"})}
         </div>
         </div>
-        <ReactMarkdown children={post.text} className='text-wrap post-body' remarkPlugins={[remarkGfm]} />
+        <ReactMarkdown children={post.content} className='text-wrap post-body' remarkPlugins={[remarkGfm]} />
     </article>
   )
 }
